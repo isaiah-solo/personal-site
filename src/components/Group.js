0b50df92493aeb0233d488c65630c93bb62aefce
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2018-05-17 18:18:53 -0700

    removed carousel temporarily until actually using, cleaned more existing css

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import '../styles/App.css';
+const GroupDiv = styled.div`
+  margin-top: ${ props => props.empty ? '0px' : '16px' };
+`;
 
 const Group = props => (
-  <div style={{marginTop: props.items.length !== 0 ? '16px' : '0px'}}> { props.items } </div>
+  <GroupDiv empty={ props.items.length === 0 }> { props.items } </GroupDiv>
 );
 
 export default Group;
--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
+import styled from 'styled-components';
 
-import '../styles/App.css'
+const IconWrapperDiv = styled.div`
+  margin: 5px 10px 5px 0px;
+  display: inline-block;
+`;
+
+const IconDiv = styled.a`
+  color: white;
+  height: 20px;
+  width: 20px;
+  text-shadow: 0 1px 0 rgba(0, 0, 0, 0.1);
+
+  &:hover {
+    color: #05b1d1;
+  }
+`;
 
 const Icon = props => (
-  <div style={{ margin: '5px 10px 5px 0px', display: 'inline-block' }}>
-    <a className='Icon' href={ props.link } target="_blank" rel="noopener">
+  <IconWrapperDiv>
+    <IconDiv href={ props.link } target="_blank" rel="noopener">
       <FontAwesome
         className={ props.name || '' }
         name={ props.name || '' }
         size='2x'
       />
-    </a>
-  </div>
+    </IconDiv>
+  </IconWrapperDiv>
 );
 
 export default Icon;
