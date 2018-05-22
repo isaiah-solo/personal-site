323387178e735c24fc58123b643e21e4c0777338
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2018-05-22 01:01:46 -0700

    added typing text component, switched functions to function statements

---
+++ src/components/TypingText.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+export default class TypeText extends React.Component {
+  state = {
+    text: "",
+  }
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (this.props.toType === prevProps.toType) return;
+    this.typeSentences();
+  }
+
+  updateLetter = newText => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.setState({ text: newText }, () => {
+          resolve(newText);
+        });
+      }, 40);
+    });
+  }
+
+  typeSentence = sentence => {
+    return new Promise(resolve => {
+      let current = 0;
+      let isBackspacing = false;
+
+      const next = (base = this.state.text) => {
+        if (isBackspacing === false && current < sentence.length) {
+          const letter = sentence[current++];
+          this.updateLetter(base + letter).then(next);
+        }
+
+        else if (base.length > 0) {
+          const backspacedBase = base.slice(0, base.length - 1);
+          setTimeout(() => {
+            isBackspacing = true;
+            this.updateLetter(backspacedBase).then(next);
+          }, isBackspacing ? 0 : 1000);
+        }
+
+        else {
+          resolve("Success");
+        }
+      };
+
+      if (sentence) next();
+    });
+  }
+
+  typeSentences = () => {
+    const phrases = this.props.toType;
+    const loop = this.props.loop;
+
+    let current = 0;
+    const next = async () => {
+      if (loop && current >= phrases.length) {
+        current %= phrases.length;
+      }
+
+      else if (!loop && current >= phrases.length) {
+        return;
+      }
+
+      this.typeSentence(phrases[current++]).then(() => {
+        setTimeout(next, 100)
+      });
+    };
+
+    if (phrases.length > 0) next();
+  }
+
+  render = () => {
+    return <span> { this.state.text + '_' } </span>;
+  }
+}
+
