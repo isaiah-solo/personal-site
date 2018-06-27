6de89287b02ebb6d45b6daa0053ce146fd1dc92d
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2018-06-26 23:05:26 -0700

    readded docker_deploy script

---
+++ docker_deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "$DOCKER_PASS" | docker login -u $DOCKER_USER --password-stdin
+export REPO=isaiahsolo/personal-site-frontend
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+docker build -f Dockerfile -t $REPO:$COMMIT .
+docker tag $REPO:$COMMIT $REPO:$TAG
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker push $REPO
